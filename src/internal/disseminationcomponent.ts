d569e8133457eaf239420ffaabc93b2f2e7ab21e
David <davidgportillo@hotmail.com>
2020-02-07 12:18:22 +0100

    dkakdg

--- src/internal/disseminationcomponent.ts
+++ src/internal/disseminationcomponent.ts
@@ -13,8 +13,8 @@ import Ball from './ball';
 export default class DisseminationComponent {
     
     // Variables algoritmo EpTO
-    private static K: number = 15;                  // Tamaño de la muestra aleatorio de procesos
-    public static TTL: number = 64;                 // Maximo numero de saltos de los eventos
+    private static K: number = 20;                  // Tamaño de la muestra aleatorio de procesos
+    public static TTL: number = 35;                 // Maximo numero de saltos de los eventos
     private static deltha: number = 200;            // Round duration in milliseconds
     private _nextBall: { [id: string]: Event; };    // Conjunto de eventos a enviar en la proxima ronda
     private _peers: Dealer[];                       // Conjunto de conexiones correctas
